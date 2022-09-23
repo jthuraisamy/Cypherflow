e878b95eb8abf5d8ee01f4b2c4fd9bf99c957d98
jxn <jxn@example.com>
2022-09-23 17:50:11 -0400

    Clean up unused code.

--- demos/TicTacToe/GameService/src/tasks/InitializeBoard.ts
+++ demos/TicTacToe/GameService/src/tasks/InitializeBoard.ts
@@ -1,5 +1,4 @@
 import { BaseTask } from '../../lib/Cypherflow/tasking/BaseTask';
-import { parseQuery } from '../../lib/Cypherflow/helpers/Cypher';
 
 export class InitializeBoard extends BaseTask {
   static spec = {
@@ -28,10 +27,6 @@ export class InitializeBoard extends BaseTask {
     // Save properties.
     this.data.output['properties'].value = JSON.stringify([...Array(9)].map((x) => 0));
     this.data.output['properties'].state = 'ONGOING';
-
-    // const ast = parseQuery(`MATCH (old:Board {value: [0,0,0,0,0,0,0,0,0], graphId: "01GCZGKJJNYH5KH2SQCRF4SSGP"})`);
-    // console.log(ast.root.body.clauses[0].pattern.paths[0].elements[0].properties.entries);
-    // console.log(ast.root.body.clauses[0].pattern.paths[0].elements[0].properties.entries.value.elements);
 
     // Print output.
     this.logTask(`=> ${JSON.stringify(this.data.output['properties'])}`);
